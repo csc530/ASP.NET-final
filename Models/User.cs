904bc9c5c724e255dfbcb998b55bba0c996098d5
csc530 <77406318+csc530@users.noreply.github.com>
2021-10-01 11:33:50 -0400

    Created basic Model classes for jobs and users

---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPFinal.Models
+{
+	public class User
+	{
+		private string name { get; set; }
+		private int ID { get; set; }
+	}
+}
---
+++ Models/jobPost.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPFinal.Models
+{
+	public class JobPost
+	{
+		private string jobName { get; set; }
+		private int jobId { get; set; }
+		private bool jobStatus { get; set; }
+		private User postedBy { get; set; }
+		private User fufilledBy { get; set; }
+	}
+}
